79ec31757c8ecfa4bc14ae98470c8a29e3eb0614
Arbaaz Khan <arbaaz9416@gmail.com>
2025-10-31 22:50:27 +0530

    It is Complete

--- WEB/FontLoom_web/apiConfig.js
+++ WEB/FontLoom_web/apiConfig.js
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config(); 
-export const API_BASE_URL = process.env.API_BASE_URL || "http://127.0.0.1:8000";
+// export const API_BASE_URL = process.env.API_BASE_URL || "http://127.0.0.1:8000";
+
+// Default: read from .env
+let baseURL = process.env.API_BASE_URL;
+
+if (process.env.DOCKER_ENV === "false") {
+  baseURL = "http://127.0.0.1:8000";
+}
+
+export const API_BASE_URL = baseURL;
--- WEB/FontLoom_web/routes/mainRoute.js
+++ WEB/FontLoom_web/routes/mainRoute.js
@@ -10,24 +10,26 @@ router.use((req, res, next) => {
 });
 
 router.get("/get-api-url", (req, res) => {
-  // res.json({ apiUrl: API_BASE_URL });
   res.json({ apiUrl: "http://localhost:8000" });
 });
+
 router.get("/", (req, res) => {
     console.log(`${new Date().toISOString()} | ${req.method} ${req.url}`);
   res.render("home", { title: "FontLoom | evokes craftsmanship and creativity", currentUrl: "/" });
 });
 
+
 router.get("/gallery", async (req, res) => {
   const content = { title: "Font Gallery | FontLoom", currentUrl: req.path,galleryData:[] };
 
   try {
+    console.log(API_BASE_URL);
+    
     const response = await fetch(`${API_BASE_URL}/api/gallery`);
     if (!response.ok) throw new Error("Failed to fetch gallery data");
 
     const apiData = await response.json();
     content.galleryData = apiData.gallery_data; 
-    console.log(apiData.gallery_data);
     
   } catch (error) {
     console.error("Error fetching gallery data:", error);
@@ -36,6 +38,7 @@ router.get("/gallery", async (req, res) => {
   res.render("gallery", content);
 });
 
+
 router.get("/personalization",(req,res)=>{
     const content = {
         title:"Personalization Picks  | FontLoom",currentUrl:req.path
@@ -43,6 +46,7 @@ router.get("/personalization",(req,res)=>{
     console.log(`${new Date().toISOString()} | ${req.method} ${req.url}`);
     res.render("personalized",content);
 });
+
 
 router.get("/recommendation",(req,res)=>{
     const content = {
